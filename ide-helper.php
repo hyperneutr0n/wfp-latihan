87599f3811d23550073bd6546d09ea266fc1d969
hyperneutr0n <randylemuel21@gmail.com>
2025-10-06 14:38:44 +0700

    making migrations

---
+++ ide-helper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Contracts\Foundation {
+    /** @mixin \Illuminate\Foundation\Application */
+    interface Application {}
+}
+
+namespace PHPUnit\Framework {
+    /** @mixin \Illuminate\Foundation\Testing\TestCase */
+    interface TestCase {}
+}
